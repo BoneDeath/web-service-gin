5d2c02e7c1803b3f3b6b58feb5eb1522dcd6dfab
BoneDeath <mhasannudin300@gmail.com>
2025-05-21 11:38:26 +0700

    added cors

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"backend/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:52242"}
+
+	router.Use(cors.New(config))
 	router.GET("/albums", handlers.GetAlbums)
 	router.GET("/products", handlers.GetProducts)
 	router.Run("localhost:8080")
